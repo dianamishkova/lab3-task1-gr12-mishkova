5b837c6536da1ba26cbc7f4203058820eba53026
dianamishkova <113173126+dianamishkova@users.noreply.github.com>
2023-02-26 16:42:41 +0000

    changed readme changed hello.c and renamed changed .gitignore

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main (void)
+{
+  float m,km,m1;
+  printf("km = ");
+  scanf("%f",&km);
+  printf("m = ");
+  scanf("%f",&m);
+  m1 = m * 3.6;
+  if(km<m1){
+    printf("%f m/s", m);
+  }
+  else if(km>m1){
+    printf("%f km/h", km)
+  }
+  else{
+    printf("equals");
+  }
+  return 0;
+}
